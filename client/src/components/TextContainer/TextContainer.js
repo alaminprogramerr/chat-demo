c3791718021b10169132ee283b14f98cc8ca4ae8
Alamin <60449971+alaminprogramerr@users.noreply.github.com>
2020-06-30 09:43:52 -0500

    demon

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h3 > <span role="img" aria-label="emoji">💬</span>Realtime Chat Application (DEMO)</h3>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h4>Joined People :</h4>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
